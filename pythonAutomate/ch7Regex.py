5cf0924af70e1d62587ed44b9401989ef9864f60
Michelle Ho <digital101ninja@gmail.com>
2015-12-23 10:12:07 -0700

    added area code extraction

--- pythonAutomate/ch7Regex.py
+++ pythonAutomate/ch7Regex.py
@@ -2,6 +2,7 @@
 
 import re
 
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
 mo = phoneNumRegex.search('My number is 415-555-4242')
 print "Phone number found:" + mo.group()
+print "Area code is:" + mo.group(1)
